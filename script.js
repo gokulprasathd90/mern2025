2a5fa59bacf49d5ea9e2c350f5551f517b974ccc
Gokul <gokulprasath90430@gmail.com>
2025-06-19 09:41:42 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var a;
+a=10;
+var a=20;
+let b;
+b=40;
+console.log(b)
+var makrs =[90,91,92,93,94];
+var [m1,m2,m3,m4,m5]=makrs;
+console.log(m1)
+console.log(m2)
+console.log(m3)
+console.log(m4)
+console.log(m5)
+var odd=Array.filter(num => num%2!=0);
+console.log(odd)
